8042e548208c7eb287a351bde00881b5a958c942
Toprak <toprakscomputer@gmail.com>
2026-05-14 20:57:18 +0300

    pencere kapatirken onaylatma

--- texteditor.py
+++ texteditor.py
@@ -1,6 +1,9 @@
 import sys
 import tkinter as tk
 from tkinter import filedialog, messagebox
+from colorama import Fore, Style, init
+
+init(autoreset=True)
 
 
 width=720
@@ -27,6 +30,13 @@ def dosyayi_kaydet():
         icerik=metin_alani.get(1.0, "end")
         with open(dosya_yolu, "w", encoding="utf-8") as dosya:
             dosya.write(icerik)
+def pencere_kapat():
+    if messagebox.askokcancel("Çıkış","Çıkmak istediğinize emin misiniz?"):
+        pencere.destroy()
+pencere.protocol("WM_DELETE_WINDOW", pencere_kapat)
+
+
+
 alt_cerceve=tk.Frame(pencere, bg='black')
 alt_cerceve.pack(side='bottom', fill='x')
 kaydet_butonu=tk.Button(alt_cerceve, bg='black', fg="#39FF14", text="Kaydet&Save", command=dosyayi_kaydet)
@@ -39,4 +49,3 @@ if len(sys.argv) > 1:
     with open(gelen_dosya, "r", encoding="utf-8") as f:
         metin_alani.insert("1.0", f.read())
 pencere.mainloop()
-
